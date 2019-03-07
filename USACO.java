139f8271633362f48f5db95f2bf9ad33f170d821
Calvin Chu <cchu00@stuy.edu>
2019-03-07 11:47:13 -0500

    classwork silver

--- USACO.java
+++ USACO.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class USACO{
   public static void main(String[] args) {
     System.out.println(bronze("makelake.in"));
+    System.out.println(silver("ctravel.in"));
   }
   public static int bronze(String filename) {
     int R = 0, C = 0, E = 0, N = 0;
@@ -13,7 +14,7 @@ public class USACO{
       File f = new File(filename);
       Scanner in = new Scanner(f);
       int line = 0;
-      while(in.hasNext()) {
+      while(in.hasNextLine()) {
         if (line == 0) {
           Scanner s = new Scanner(in.nextLine());
           int index = 0;
@@ -88,6 +89,58 @@ public class USACO{
     return totalDepth * 5184;
   }
   public static int silver(String filename) {
+    int N = 0, M = 0, T = 0, R1 = 0, C1 = 0, R2 = 0, C2 = 0;
+    char[][] map = new char[0][0];
+    try {
+      File f = new File(filename);
+      Scanner in = new Scanner(f);
+      int line = 0;
+      while (in.hasNextLine()) {
+        if (line == 0) {
+          Scanner s = new Scanner(in.nextLine());
+          int index = 0;
+          while (s.hasNextInt()) {
+            if (index == 0) N = s.nextInt();
+            if (index == 1) M = s.nextInt();
+            if (index == 2) T = s.nextInt();
+            index++;
+          }
+          map = new char[N][M];
+        }
+        if (line > 0 && line < N + 1) {
+          String s = in.nextLine();
+          int index = 0;
+          for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+              map[line - 1][index] = s.charAt(i);
+              index++;
+            }
+          }
+        }
+        if (line == N + 1) {
+          Scanner s = new Scanner(in.nextLine());
+          int index = 0;
+          while (s.hasNextInt()) {
+            if (index == 0) R1 = s.nextInt();
+            if (index == 1) C1 = s.nextInt();
+            if (index == 2) R2 = s.nextInt();
+            if (index == 3) C2 = s.nextInt();
+            index++;
+          }
+        }
+        line++;
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return 0;
+    }
+
+    for (int r = 0; r < map.length; r++) {
+      for (int c = 0; c < map[r].length; c++) {
+        System.out.print(map[r][c] + " ");
+      }
+      System.out.println();
+    }
     return 0;
   }
 }
